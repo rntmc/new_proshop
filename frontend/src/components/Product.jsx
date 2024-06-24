b3028a32e8b5caaece8c032a1c63a4e64f7ace11
rntmc <carrino.renato@gmail.com>
2024-06-24 13:59:08 -0300

    rating and product screen

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import {Card} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
@@ -10,10 +11,15 @@ const Product = ({product}) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className='product-title'>{/*product-title aplica elipse no nome, para ficarem todos do mesmo tamanho*/}
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </Card.Text>
+
         <Card.Text as="h3">
           ${product.price}
         </Card.Text>
