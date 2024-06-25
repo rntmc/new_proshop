0bba7bb72731fc61ad8b1e43a5627ea68d5c0510
rntmc <carrino.renato@gmail.com>
2024-06-25 16:28:57 -0300

    backend authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,6 +23,19 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next){ //'pre' allow us to do something before we save it to the database(in our case, encrypt the password)
+  if(!this.isModified('password')) { //if we are not dealing with PW we will move on
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+}) 
 
 const User = mongoose.model("User", userSchema);
 
